5acbb22d183d8ca14752191b24a94a779853e726
hkbharath <hegdebharathkumar@gmail.com>
2024-09-01 00:02:02 +0100

    Extended MARL test with steer_vel and without heading

--- highway_env/envs/merge_env_v1.py
+++ highway_env/envs/merge_env_v1.py
@@ -371,7 +371,32 @@ class MergeEnvLCMARL(MergeEnv):
                                             road=road, 
                                             position = position, 
                                             speed=speed)
+
+class MergeEnvMARLSteerVel(MergeEnvLCMARL):
+    n_a = 5
+    n_s = 25
     
+    @classmethod
+    def default_config(cls) -> dict:
+        config = super().default_config()
+        config.update({
+            "action": {
+                "type": "MultiAgentAction",
+                "action_config": {
+                    "type": "DiscreteMetaActionLC",
+                    "lateral": True,
+                    "longitudinal": True
+                }},
+            "observation": {
+                "type": "MultiAgentObservation",
+                "observation_config": {
+                    "type": "Kinematics"
+                }},
+            "controlled_vehicles": 4,
+            "lateral_control": "steer"
+        })
+        return config
+
 register(
     id='merge-v1',
     entry_point='highway_env.envs:MergeEnv',
@@ -385,4 +410,9 @@ register(
 register(
     id='merge-multi-agent-v1',
     entry_point='highway_env.envs:MergeEnvLCMARL',
+)
+
+register(
+    id='merge-multi-agent-v0.5',
+    entry_point='highway_env.envs:MergeEnvMARLSteerVel',
 )
