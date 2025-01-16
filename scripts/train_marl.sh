141dd972e3c15953c07130584fafc13b91b3cb1c
hkbharath <hegdebharathkumar@gmail.com>
2025-01-16 21:26:09 +0000

    Update training script to run parallelly

--- scripts/train_marl.sh
+++ scripts/train_marl.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-screen python run_mappo.py --config $1
+sname==$(echo "$1" | awk -F'/' '{print $NF}')
+screen -dmS "$sname-0" python run_mappo.py --config $1
+sleep 5
 sed -i 's/\bseed = 0\b/seed = 2000/g' $1
-screen python run_mappo.py --config $1
+screen -dmS "$sname-2000" python run_mappo.py --config $1
+sleep 5
 sed -i 's/\bseed = 2000\b/seed = 2024/g' $1
-screen python run_mappo.py --config $1
+screen -dmS "$sname-2024" python run_mappo.py --config $1
+sleep 5
 sed -i 's/\bseed = 2024\b/seed = 0/g' $1
