c0b6e10e716bdd7e78949f8143ac744fc61a9bb9
hkbharath <hegdebharathkumar@gmail.com>
2025-02-06 10:29:34 +0000

    Update training script with two options for random seeds

--- scripts/train_marl.sh
+++ scripts/train_marl.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sname==$(echo "$1" | awk -F'/' '{print $NF}')
 old_seed=0
-# screen -dmS "$sname-0" python run_mappo.py --config $1
-# sleep 5
+screen -dmS "$sname-0" python run_mappo.py --config $1
+sleep 5
 
-# rand_seeds=(2000 2024 123 4567 890 2743 1598 3621 490 0)
-rand_seeds=(123 4567 890 2743 1598 3621 490 0)
+rand_seeds_3=(2000 2024 0)
+rand_seeds_10=(2000 2024 123 4567 890 2743 1598 3621 490 0)
+rand_seeds=$rand_seeds_3
 
 for seed in "${rand_seeds[@]}"; do
     sleep 5
