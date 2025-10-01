749841701be23e549efa51d0434d1726c1f00755
Bharathkumar Hegde <hegdebharathkumar@gmail.com>
2025-10-01 11:50:02 +0000

    cal evaluation script

---
+++ scripts/evalution_marl-MASS-shared-cal.sh
@@ -0,0 +1,16 @@
+# TD 1
+
+# TD 2
+
+# TD 3
+python run_mappo.py --opt eval --model-dir "/home/jovyan/work/cbf-cav-mixed/MARL-MASS/results/Sep_29_16_10_25" --exp-name marl_cav-heading-t_headway-cbf-cav-td3-mixed-srew-lr:1e4-shared-cal --src-url https://wandb.ai/ai-driver/Safe_mixed_MARL/runs/vy8suobz --checkpoint 15600 &
+
+python run_mappo.py --opt eval --model-dir "/home/jovyan/work/cbf-cav-mixed/MARL-MASS/results/Sep_29_16_10_19" --exp-name marl_cav-heading-t_headway-cbf-cav-td3-mixed-srew-lr:1e4-shared-cal --src-url https://wandb.ai/ai-driver/Safe_mixed_MARL/runs/7omogi87 --checkpoint 15600 &
+
+python run_mappo.py --opt eval --model-dir "/home/jovyan/work/cbf-cav-mixed/MARL-MASS/results/Sep_29_16_10_14" --exp-name marl_cav-heading-t_headway-cbf-cav-td3-mixed-srew-lr:1e4-shared-cal --src-url https://wandb.ai/ai-driver/Safe_mixed_MARL/runs/ngx8snnd --checkpoint 15600 &
+
+python run_mappo.py --opt eval --model-dir "/home/jovyan/work/cbf-cav-mixed/MARL-MASS/results/Sep_29_16_10_09" --exp-name marl_cav-heading-t_headway-cbf-cav-td3-mixed-srew-lr:1e4-shared-cal --src-url https://wandb.ai/ai-driver/Safe_mixed_MARL/runs/1h0ge9ff --checkpoint 15600 &
+
+python run_mappo.py --opt eval --model-dir "/home/jovyan/work/cbf-cav-mixed/MARL-MASS/results/Sep_29_16_09_59" --exp-name marl_cav-heading-t_headway-cbf-cav-td3-mixed-srew-lr:1e4-shared-cal --src-url https://wandb.ai/ai-driver/Safe_mixed_MARL/runs/3juubk3y --checkpoint 15600 &
+
+# TD 3 - Curriculum Learning - Mixed Traffic - MARL-MASS
